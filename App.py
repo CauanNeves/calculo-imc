4508c4ac666066460e950166ac92aac61fbbdccb
Cauan Neves <c.neves8903@gmail.com>
2024-12-20 21:53:55 -0300

    2.8

--- App.py
+++ App.py
@@ -2,13 +2,16 @@ import PySimpleGUI as sg
 
 #tema
 sg.theme('BrownBlue')
+
 #layout
+#Cabeçalho tabela 
 top_row= ['IMC', 'Classificações']
+#Linhas tabela
 rows= [
     ['< 17', 'Muito Abaixo do Peso'],
     ['17 - 18,5', 'Abaixo do Peso'],
     ['18,5 - 24,9', 'Peso Normal'],
-    ['25 - 29,9', 'Acima do peso'],
+    ['25 - 29,9', 'Acima do weight'],
     ['30 - 34,9', 'Obesidade I '],
     ['35 - 39,9', 'Obesidade II (severa)'],
     ['> 40', 'Obesidade III (mórbida)']
@@ -16,13 +19,14 @@ rows= [
 
 #Separando a aba IMC
 frame_calc= [
-    [sg.Text('Altura (m): '), sg.Input(key='altura', size=(5, 1)), 
-     sg.Text('Peso (kg): '), sg.Input(key='peso', size=(5, 1)), 
-     sg.Button('Calcular', key='btn_calc')],
+    [sg.Text('Altura (m): '), sg.Input(key='height', size=(5, 1)), 
+     sg.Text('Peso (kg): '), sg.Input(key='weight', size=(5, 1)), 
+     sg.Button('Calcular', key='btn_calc_imc')],
 
     [sg.Text(key='result', expand_x=True, justification='center')]
 ]
 
+#tabela
 tab_imc = [
     [sg.Table(values=rows, headings=top_row, justification='center', 
               hide_vertical_scroll=True, expand_x=True, num_rows=7, row_height=25)],
@@ -30,16 +34,23 @@ tab_imc = [
 
 ]
 
-tab_txbasal= [
-    [sg.Column(
-        [[sg.Text('Em breve', text_color= 'yellow')]], 
-        justification='center',  
-        element_justification='center', 
-        expand_x=True, 
-        expand_y=True
-    )]
+#Taxa Basal
+frame_result_tb= [
+    [sg.Text('Este é o seu gasto calórico basal: '), sg.Text('', key= 'result_tb')],
 ]
 
+tab_txbasal= [
+    [sg.Text('Selecione o seu gênero:')],
+    [sg.Radio('Homem', group_id= 'sexo', key= 'radio_male', default= True), sg.Radio('Mulher', group_id= 'sexo', key= 'radio_female')],
+    [sg.Text('Altura (m):'), sg.Input(key='height_tb', size=(5, 1)), 
+     sg.Text('Peso (kg):'), sg.Input(key='weight_tb', size=(5, 1)),
+     sg.Text('Idade:'), sg.Input(key= 'age_tb', size= (5, 1))
+     ],
+    [sg.Button('Calcular', key= 'btn_calc_tb', expand_x= True)],
+    [sg.Frame('Resultado', frame_result_tb, expand_x= True, expand_y= True)]
+]
+
+#Layout Principal
 main_layout= [
     [sg.TabGroup([
         #Uma lista para cada tab
@@ -49,19 +60,19 @@ main_layout= [
 ]
 
 #Janela
-window= sg.Window('Calculadora de IMC', layout= main_layout, element_justification='c', resizable=True)
+window= sg.Window('Calculadora de IMC', layout= main_layout, element_justification='c')
 #leitura de eventos e valores
 while True:
     event, values = window.read()
     #ler e reagir
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'btn_calc':
+    elif event == 'btn_calc_imc':
         try:
             window['result'].update('')
-            altura = float(values['altura'].replace(',', '.'))
-            peso = float(values['peso'].replace(',', '.'))
-            imc = peso / (altura ** 2)
+            height = float(values['height'].replace(',', '.'))
+            weight = float(values['weight'].replace(',', '.'))
+            imc = weight / (height ** 2)
 
             imc_ranges = [
                 (0, 17, 'medium slate blue'),
@@ -81,3 +92,22 @@ while True:
             
         except ValueError:
             window['result'].update('Os campos acima não podem ser vazios', text_color= 'red')
+            
+    elif event == 'btn_calc_tb':
+        try:
+            height = float(values['height_tb'].replace(',', '.')) * 100
+            weight = float(values['weight_tb'].replace(',', '.'))
+            age= float(values['age_tb'].replace(',', '.'))
+            
+            if values['radio_male'] == True:
+                tmb_male= 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+                window['result_tb'].update(f'{tmb_male:,.2f}', text_color= 'green', background_color= 'white')
+            else:
+                tmb_female = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
+                window['result_tb'].update(f'{tmb_female:,.2f}', text_color= 'green', background_color= 'white')
+        except:
+            sg.popup('Os campos não podem ser vazios')
+
+
+
+#O ideal, para calcular o peso, é que seja em jejum logo pela manhã. Calcule todos os dias durante uma semana e depois tire uma média.
